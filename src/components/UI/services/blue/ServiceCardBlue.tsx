9720ef6c577107ae12738f4cb38013fc6860015d
Aida Vafina <vai2706@bk.ru>
2025-03-15 21:43:22 +0300

    added a block of services

---
+++ src/components/UI/services/blue/ServiceCardBlue.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./ServiceCardBlue.module.scss";
+
+type ServiceCardProps = {
+    title: string;
+    description: string;
+    tools: string[];
+    imageSrc: string;
+};
+
+const ServiceCardBlue: React.FC<ServiceCardProps> = ({ title, description, tools, imageSrc}) => {
+    return (
+        <div className={styles.cardWrapper}>
+            <div className={styles.card}>
+                <div className={styles.content}>
+                    <h3 className={styles.title}>{title}</h3>
+                    <p className={styles.description}>{description}</p>
+                    <div className={styles.tools}>
+                        {tools.map((tool, index) => (
+                            <span key={index} className={styles.tool}>{tool}</span>
+                        ))}
+                    </div>
+                    <img src={imageSrc} alt={title} className={styles.image} />
+                </div>
+            </div>
+            <button className={styles.button}>Подробнее</button>
+        </div>
+    );
+};
+
+export default ServiceCardBlue;
--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,11 +1,59 @@
 import React from "react";
 import styles from "./Services.module.scss";
+import ServiceCardBlue from "../UI/services/blue/ServiceCardBlue.tsx";
+import image from "../../assets/images/service-card.png"
+import image2 from "../../assets/images/service-card-grey.png"
+import ServiceCardLight from "../UI/services/light/ServiceCardLight.tsx";
 
 const Services: React.FC = () => {
     return (
         <section id="services" className={styles.services}>
             <div className={styles.container}>
-                <h2>блок услуг</h2>
+                <div className={styles.header}>
+                    <button className={styles.category}>Услуги</button>
+                    <h2>Предлагаем решения профессионального уровня</h2>
+                </div>
+                <div className={styles.cards}>
+                    <ServiceCardBlue
+                        title="Дизайн и UX"
+                        description="Профессиональная цифровая упаковка для бизнеса"
+                        tools={["Figma", "Sketch", "3D", "Иллюстрации", "AI"]}
+                        imageSrc= {image}
+                    />
+                    <ServiceCardBlue
+                        title="Дизайн и UX"
+                        description="Профессиональная цифровая упаковка для бизнеса"
+                        tools={["Figma", "Sketch", "3D", "Иллюстрации", "AI"]}
+                        imageSrc= {image}
+                    />
+                    <ServiceCardLight
+                        title="Дизайн и UX"
+                        description="Профессиональная цифровая упаковка для бизнеса"
+                        tools={["Figma", "Sketch", "3D", "Иллюстрации", "AI"]}
+                        imageSrc= {image2}
+                    />
+
+                    <ServiceCardLight
+                        title="Дизайн и UX"
+                        description="Профессиональная цифровая упаковка для бизнеса"
+                        tools={["Figma", "Sketch", "3D", "Иллюстрации", "AI"]}
+                        imageSrc= {image2}
+                    />
+
+                    <ServiceCardLight
+                        title="Дизайн и UX"
+                        description="Профессиональная цифровая упаковка для бизнеса"
+                        tools={["Figma", "Sketch", "3D", "Иллюстрации", "AI"]}
+                        imageSrc= {image2}
+                    />
+
+                    <ServiceCardLight
+                        title="Дизайн и UX"
+                        description="Профессиональная цифровая упаковка для бизнеса"
+                        tools={["Figma", "Sketch", "3D", "Иллюстрации", "AI"]}
+                        imageSrc= {image2}
+                    />
+                </div>
             </div>
         </section>
     );
